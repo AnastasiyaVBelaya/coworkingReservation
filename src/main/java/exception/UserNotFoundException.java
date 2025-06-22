75d558bee9df00a9db12dd6b180ca556138bb402
Anastasiya <anastasia-belaya@mail.ru>
2025-06-22 19:38:11 +0300

    add new custom exception

--- src/main/java/exception/UserNotFoundException.java
+++ src/main/java/exception/UserNotFoundException.java
@@ -1,2 +1,14 @@
-package exception;public class UserNotFoundException {
+package exception;
+
+public class UserNotFoundException extends RuntimeException {
+    private final String login;
+
+    public UserNotFoundException(String login) {
+        super("User with login " + login + " not found");
+        this.login = login;
+    }
+
+    public String getLogin() {
+        return login;
+    }
 }
